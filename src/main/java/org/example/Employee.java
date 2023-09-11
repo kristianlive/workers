6bfd182279f180f029566a539c9010c32a2ec5ab
kristianlive <kristianlive@hotmail.com>
2023-09-11 17:56:00 +0200

    Added class Menu and Employee, Methoths for controll and info

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Employee {
 
     public String name;
@@ -19,7 +21,7 @@ public class Employee {
     public Employee(String name, int id,String gender,String endDate,String credentials){
         System.out.println("Object Created");
         setValuesTrainee(name,id,gender,endDate,credentials);
-        System.out.println(getValuesTrainee());
+        System.out.println(getValuesAll());
     }
 
 
@@ -48,9 +50,75 @@ public class Employee {
         this.credentials = credentials;
     }
 
-    public String getValuesTrainee(){
-        String info = "Employee name:"+name + ". Id:"+id+". Gender:"+gender +". EndDate:" + endDate+". Credentials:"+ credentials;
-        return info;
+
+    public int getSalary(){
+        String info = "Salary Info: " + salary;
+        return salary;
     }
 
-}
+    public String getValuesAll() {
+
+        StringBuilder info = new StringBuilder("Employee name: " + name + ". Id:" + id + ". Gender:" + gender);
+
+        if (salary != 0) {
+            info.append(". Salary:" + salary);
+        }
+
+        if (startDate != null) {
+            info.append(". Start Date:" + startDate);
+        }
+
+        if (endDate != null) {
+            info.append(". End Date:" + endDate);
+        }
+
+        if (credentials != null) {
+            info.append(". Credentials: " + credentials);
+        }
+
+        return info.toString();
+    }
+
+    public static int calculateAverageSalaryForWomen (ArrayList <Employee> employees) {
+        int totalSalaryWomen = 0;
+        int countWomen = 0;
+
+        for (Employee employee : employees) {
+            int infoSalary = employee.getSalary();
+            if ("Female".equals(employee.gender) && infoSalary > 0) {
+                totalSalaryWomen += infoSalary;
+                countWomen++;
+            }
+        }
+
+
+        if (countWomen == 0) {
+            return 0;
+        } else {
+            return totalSalaryWomen / countWomen;
+        }
+    }
+
+    public static int calculateAverageSalaryForMen(ArrayList<Employee> employees) {
+        int totalSalaryMen = 0;
+        int countMen = 0;
+
+        for (Employee employee : employees) {
+            int infoSalary = employee.getSalary();
+            if ("Male".equals(employee.gender) && infoSalary > 0) {
+                totalSalaryMen += infoSalary;
+                countMen++;
+            }
+        }
+
+        if (countMen == 0) {
+            return 0;
+        } else {
+            return totalSalaryMen / countMen;
+        }
+
+
+        }
+
+
+    }
